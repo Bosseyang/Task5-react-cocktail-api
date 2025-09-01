1389a0f2e4037991ca95430f544ebf74972042ee
Bosseyang <bosse_yang@hotmail.com>
2025-09-01 16:18:59 +0200

    Create LandingPage.tsx

---
+++ src/pages/LandingPage.tsx
@@ -0,0 +1,10 @@
+import type { ReactElement } from "react";
+
+export const LandingPage: React.FC = () => {
+  return (
+    <main className="main landing-page">
+      <h2 className="h2 landing-page-h2">Random Cocktail</h2>
+      
+    </main>
+  );
+};
