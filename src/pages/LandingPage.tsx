9bec2fa01574d5268a3221cc4789d3fd2e0c1407
Bosseyang <bosse_yang@hotmail.com>
2025-09-02 14:58:18 +0200

    Update LandingPage.tsx

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,10 +1,30 @@
-import type { ReactElement } from "react";
+import { useEffect, useState } from "react";
+import { CocktailCard } from "../components/CocktailCard";
+import type { ICocktail } from "../types/types";
+import { getRandomCocktail } from "../cocktailApi";
+import { Loader } from "../components/Loader";
 
 export const LandingPage: React.FC = () => {
+  const [cocktail, setCocktail] = useState<ICocktail | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
+
+  async function fetchRandom() {
+    setLoading(true);
+    const drink = await getRandomCocktail();
+    console.log(drink);
+    setCocktail(drink);
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    fetchRandom();
+  }, []);
+
   return (
     <main className="main landing-page">
       <h2 className="h2 landing-page-h2">Random Cocktail</h2>
-      
+      {loading && <Loader />}
+      {cocktail && <CocktailCard cocktail={cocktail} classStyle={"landing-page-image"} />}
     </main>
   );
 };
