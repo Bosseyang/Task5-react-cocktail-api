ff581ff54a235c3c227bc4d466b48c6a4006a258
Bosseyang <bosse_yang@hotmail.com>
2025-09-02 17:03:58 +0200

    Update CocktailInfoPage.tsx

--- src/pages/CocktailInfoPage.tsx
+++ src/pages/CocktailInfoPage.tsx
@@ -4,7 +4,7 @@ import { Loader } from "../components/Loader";
 import { CocktailImage } from "../components/CocktailImage";
 import { CocktailIngredients } from "../components/CocktailIngredients";
 import { useParams } from "react-router";
-import { getCocktailById } from "../cocktailApi";
+import { getCocktailById } from "../services/cocktailApi";
 
 export const CocktailInfoPage: React.FC = () => {
   const { id } = useParams<{ id: string }>();
@@ -24,14 +24,22 @@ export const CocktailInfoPage: React.FC = () => {
   if (!cocktail) return <p>No cocktail was found.</p>;
   return (
     <section className="section cocktail-info-page">
-      <div className="image-category">
-        <CocktailImage cocktail={cocktail} classStyle="info-page-image" />
-        <p className="info-p">Category: {cocktail.category}</p>
-        <p className="info-p">Alcoholic: {cocktail.alcoholic ? "Yes" : "No"}</p>
-        <p className="info-p">Glass: {cocktail.glass}</p>
+      <h2 className="info-page-h2">{cocktail.name}</h2>
+      <div className="content info-page-wrapper">
+        <div className="image-category">
+          <CocktailImage cocktail={cocktail} classStyle="info-page-image" />
+          <p className="info-p">
+            <strong>Category:</strong> {cocktail.category}
+          </p>
+          <p className="info-p">
+            <strong>Alcoholic:</strong> {cocktail.alcoholic ? "Yes" : "No"}
+          </p>
+          <p className="info-p">
+            <strong>Glass:</strong> {cocktail.glass}
+          </p>
+        </div>
+        <CocktailIngredients cocktail={cocktail} />
       </div>
-      {/* <p className="info-p">Tags: {cocktail.tags}</p> */}
-      <CocktailIngredients cocktail={cocktail} />
     </section>
   );
 };
