4841685e148b04c31a0cfa0e4f5e30884c08630a
Bosseyang <bosse_yang@hotmail.com>
2025-09-02 08:58:46 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import { Header } from "./components/Header";
 import "./css/index.css";
+import { Outlet } from "react-router";
 
-function App() {
-  return <></>;
+export function App() {
+  return (
+    <>
+      <Header />
+      <Outlet />
+    </>
+  );
 }
 
 export default App;
