732dc1c6ed952384938dd0f70521a7dcc856d7c8
Bosseyang <bosse_yang@hotmail.com>
2025-09-02 14:58:14 +0200

    Create CocktailInfoPage.tsx

---
+++ src/pages/CocktailInfoPage.tsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+import type { ICocktail } from "../types/types";
+import { Loader } from "../components/Loader";
+import { CocktailImage } from "../components/CocktailImage";
+import { CocktailIngredients } from "../components/CocktailIngredients";
+import { useParams } from "react-router";
+import { getCocktailById } from "../cocktailApi";
+
+export const CocktailInfoPage: React.FC = () => {
+  const { id } = useParams<{ id: string }>();
+  const [cocktail, setCocktail] = useState<ICocktail | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (!id) return;
+    setLoading(true);
+    getCocktailById(id).then((drink) => {
+      setCocktail(drink);
+      setLoading(false);
+    });
+  }, [id]);
+
+  if (loading) return <Loader />;
+  if (!cocktail) return <p>No cocktail was found.</p>;
+  return (
+    <section className="section cocktail-info-page">
+      <CocktailImage cocktail={cocktail} />
+      <p className="info-p">Category: {cocktail.category}</p>
+      <p className="info-p">Glass: {cocktail.glass}</p>
+      <p className="info-p">Alcoholic: {cocktail.alcoholic ? "Yes" : "No"}</p>
+      <p className="info-p">Tags: {cocktail.tags}</p>
+      <CocktailIngredients cocktail={cocktail} />
+    </section>
+  );
+};
