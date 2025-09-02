c9974751ff67b6ea95a08ce6b92717aff8baddb8
Bosseyang <bosse_yang@hotmail.com>
2025-09-02 16:10:56 +0200

    Update LandingPage.tsx
    move logic out to useRandomCocktail custom hook.
    Implement custom hook

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,28 +1,15 @@
-import { useEffect, useState } from "react";
+import { useRandomCocktail } from "../hooks/useRandomCocktail";
 import { CocktailCard } from "../components/CocktailCard";
-import type { ICocktail } from "../types/types";
-import { getRandomCocktail } from "../cocktailApi";
 import { Loader } from "../components/Loader";
 
 export const LandingPage: React.FC = () => {
-  const [cocktail, setCocktail] = useState<ICocktail | null>(null);
-  const [loading, setLoading] = useState<boolean>(false);
-
-  async function fetchRandom() {
-    setLoading(true);
-    const drink = await getRandomCocktail();
-    console.log(drink);
-    setCocktail(drink);
-    setLoading(false);
-  }
-
-  useEffect(() => {
-    fetchRandom();
-  }, []);
+  const { cocktail, loading, fetchRandom } = useRandomCocktail();
 
   return (
     <main className="main landing-page">
-      <h2 className="h2 landing-page-h2">Random Cocktail</h2>
+      <button onClick={fetchRandom}>
+        <h2 className="h2 landing-page-h2">Random Cocktail</h2>
+      </button>
       {loading && <Loader />}
       {cocktail && <CocktailCard cocktail={cocktail} classStyle={"landing-page-image"} />}
     </main>
