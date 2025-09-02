bc132046a255de019e6505cf4eb36cfd060b96db
Bosseyang <bosse_yang@hotmail.com>
2025-09-02 08:58:38 +0200

    Create Header.tsx

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+
+
+export const Header: React.FC = () => {
+  return (
+    <header className="header">
+      <h1 className="h1 header-h1">Cocktails</h1>
+      <nav></nav>
+    </header>
+    
+  );
+};
