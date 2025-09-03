d77f77a5c9a8ed8f5196c727afc3a1bee96d7371
Bosseyang <bosse_yang@hotmail.com>
2025-09-03 09:02:01 +0200

    Create CocktailInfo.tsx
    rename

---
+++ src/components/CocktailInfo.tsx
@@ -0,0 +1,24 @@
+import type { ICocktail } from "../types/types";
+
+interface IInfoProps {
+  cocktail: ICocktail;
+}
+
+export const CocktailIngredients: React.FC<IInfoProps> = ({ cocktail }: IInfoProps) => {
+  return (
+    <div className="ingredients-wrapper">
+      <h2 className="ingredients-h2">Ingredients</h2>
+      <ul className="ingredients-list">
+        {cocktail.ingredients.map((ing, index) => (
+          <li key={index}>
+            {ing.ingredient} {ing.measure && `- ${ing.measure}`}
+          </li>
+        ))}
+      </ul>
+      <div className="instructions-wrapper">
+        <h2 className="instructions-h2">Instructions</h2>
+        <p className="instructions-p">{cocktail.instructions}</p>
+      </div>
+    </div>
+  );
+};
