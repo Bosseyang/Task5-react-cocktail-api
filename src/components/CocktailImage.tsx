27d785adec911fee4b6c2e7761356fbbc853346d
Bosseyang <bosse_yang@hotmail.com>
2025-09-03 18:15:36 +0200

    Update CocktailImage.tsx

--- src/components/CocktailImage.tsx
+++ src/components/CocktailImage.tsx
@@ -2,11 +2,11 @@ import type { ICocktail } from "../types/types";
 
 interface IImageProps {
   cocktail: ICocktail;
-  classStyle?: string | undefined;
 }
-export const CocktailImage: React.FC<IImageProps> = ({ cocktail, classStyle }) => {
+
+export const CocktailImage: React.FC<IImageProps> = ({ cocktail }) => {
   return (
-    <div className={`image-wrapper ${classStyle}`}>
+    <div className={`image-wrapper landing-page-image list-item-image`}>
       <img className="cocktail-card-img" src={cocktail.thumbnail} alt={cocktail.name} />
     </div>
   );
