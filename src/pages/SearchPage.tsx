b2d12b9b4e581057f278ca15d50d93772a01ba29
Bosseyang <bosse_yang@hotmail.com>
2025-09-03 16:30:21 +0200

    Create SearchPage.tsx

---
+++ src/pages/SearchPage.tsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import { SearchForm } from "../components/SearchForm";
+import { searchCocktailsByName } from "../services/cocktailApi";
+import type { ICocktail } from "../types/types";
+
+export const SearchPage: React.FC = () => {
+  const [loading, setLoading] = useState<boolean>(false);
+  const [query, setQuery] = useState<string>("");
+  const [results, setResults] = useState<ICocktail[]>([]);
+
+  async function handleSearch(q: string) {
+    if (!q.trim()) return;
+    setLoading(true);
+    const cocktails = await searchCocktailsByName(q);
+    setResults(cocktails);
+    setQuery(q);
+    setLoading(false);
+  }
+
+  return (
+    <section className="search-page">
+      <h1 className="search-page-h1"></h1>
+      <SearchForm onSearch={handleSearch} />
+    </section>
+  );
+};
