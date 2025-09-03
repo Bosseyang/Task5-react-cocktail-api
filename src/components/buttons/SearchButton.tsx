e1e5d43f64d879a9f0e73d0f235f13aaf7553013
Bosseyang <bosse_yang@hotmail.com>
2025-09-03 16:27:11 +0200

    Create SearchButton.tsx
    add search button

---
+++ src/components/buttons/SearchButton.tsx
@@ -0,0 +1,7 @@
+export const SearchButton: React.FC = () => {
+  return (
+    <button type="submit" className="button search-button">
+      <span className="material-symbols-outlined">search</span> Search
+    </button>
+  );
+};
