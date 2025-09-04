5d51abdd899f10788757b1bd55be29e900b44d80
Bosseyang <bosse_yang@hotmail.com>
2025-09-04 16:45:46 +0200

    Create SearchOptionsButton.tsx
    add button component

---
+++ src/components/buttons/SearchOptionsButton.tsx
@@ -0,0 +1,24 @@
+interface ISearchOptionsButtonProps {
+  icon?: string;
+  onClick?: () => void;
+  disabled?: boolean;
+  title?: string;
+  name?: string;
+}
+export const SearchOptionsButton: React.FC<ISearchOptionsButtonProps> = ({
+  onClick,
+  disabled,
+  title,
+  name,
+}) => {
+  return (
+    <button
+      onClick={onClick}
+      disabled={disabled}
+      className={`search-options-button ${disabled ? "disabled" : ""}`}
+      title={title}
+    >
+      <p>{name}</p>
+    </button>
+  );
+};
