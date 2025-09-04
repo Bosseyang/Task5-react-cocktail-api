70d351e736623ab68b6550cee4095044a1e8443f
Bosseyang <bosse_yang@hotmail.com>
2025-09-04 15:37:30 +0200

    Update IconButton.tsx
    add props with more customization

--- src/components/buttons/IconButton.tsx
+++ src/components/buttons/IconButton.tsx
@@ -1,13 +1,26 @@
 interface IIConButtonProps {
   icon: string;
-  onClick: () => void;
+  onClick?: () => void;
   disabled?: boolean;
   title?: string;
+  name?: string;
 }
-export const IconButton: React.FC<IIConButtonProps> = ({ icon, onClick, disabled, title }) => {
+export const IconButton: React.FC<IIConButtonProps> = ({
+  icon,
+  onClick,
+  disabled,
+  title,
+  name,
+}) => {
   return (
-    <button onClick={onClick} disabled={disabled} className="custom-button" title={title}>
+    <button
+      onClick={onClick}
+      disabled={disabled}
+      className={`custom-button ${disabled ? "disabled" : ""}`}
+      title={title}
+    >
       <span className="material-symbols-outlined">{icon}</span>
+      <h2>{name}</h2>
     </button>
   );
 };
