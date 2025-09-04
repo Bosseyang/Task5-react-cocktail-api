2e142a4339c6e18588e4216314fc2ace2a6d8aed
Bosseyang <bosse_yang@hotmail.com>
2025-09-04 11:18:23 +0200

    Update SearchPage.tsx

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,29 +1,21 @@
-import { useState } from "react";
 import { SearchForm } from "../components/SearchForm";
-import { searchCocktailsByName } from "../services/cocktailApi";
-import type { ICocktail } from "../types/types";
 import { CocktailList } from "../components/CocktailList";
+import { Loader } from "../components/Loader";
+import { useCocktailSearch } from "../hooks/useCocktailSearch";
+import { Pagination } from "../components/Pagination";
 
 export const SearchPage: React.FC = () => {
-  const [loading, setLoading] = useState<boolean>(false);
-  const [query, setQuery] = useState<string>("");
-  const [results, setResults] = useState<ICocktail[]>([]);
-
-  async function handleSearch(q: string) {
-    if (!q.trim()) return;
-    setLoading(true);
-    const cocktails = await searchCocktailsByName(q);
-    setResults(cocktails);
-    setQuery(q);
-    setLoading(false);
-  }
+  const { handleSearch, loading, page, setPage, totalPages, paginated } = useCocktailSearch();
 
   return (
     <section className="search-page">
       <h1 className="search-page-h1"></h1>
       <SearchForm onSearch={handleSearch} />
-
-      <CocktailList cocktails={results} />
+      {loading && <Loader />}
+      <div className={`load-wrapper ${loading ? "loading" : ""}`}>
+        <CocktailList cocktails={paginated} />
+      </div>
+      <Pagination page={page} totalPages={totalPages} onPageChange={setPage} />
     </section>
   );
 };
