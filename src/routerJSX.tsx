e5d798090fcb4fa9ae107d7fae2309d3235ccb4b
Bosseyang <bosse_yang@hotmail.com>
2025-09-04 11:18:34 +0200

    Update routerJSX.tsx
    code cleanup

--- src/routerJSX.tsx
+++ src/routerJSX.tsx
@@ -1,27 +1,13 @@
 import { createBrowserRouter, createRoutesFromElements, Route } from "react-router";
-import { App } from "./App";
+import { App } from "./components/App";
 import { LandingPage } from "./pages/LandingPage";
 import { CocktailInfoPage } from "./pages/CocktailInfoPage";
 import { SearchPage } from "./pages/SearchPage";
-
-// Data router with object routes.
-// export const router = createBrowserRouter([
-//   {
-//     path: '/',
-//     element: <App />,
-//     children: [
-//       { index: true, element: <HomeView /> },
-//       { path: ':pokemonName', element: <PokemonView /> },
-//     ],
-//   },
-// ]);
 
 // Data router with JSX routes.
 export const routerJSX = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
-      {/* <Route index element={<HomeView />} />
-      <Route path=":pokemonName" element={<PokemonView />} /> */}
       <Route index element={<LandingPage />}>
         {/* <Route path=/> */}
       </Route>
