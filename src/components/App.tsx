4b2e4b8e830a4402e8d11528f6b063f7db6747f9
Bosseyang <bosse_yang@hotmail.com>
2025-09-04 08:58:06 +0200

    Create App.tsx
    move app

---
+++ src/components/App.tsx
@@ -0,0 +1,14 @@
+import { Header } from "./Header";
+import "../css/index.css";
+import { Outlet } from "react-router";
+
+export function App() {
+  return (
+    <>
+      <Header />
+      <Outlet />
+    </>
+  );
+}
+
+export default App;
