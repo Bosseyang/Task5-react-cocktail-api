949e05be924ea3fe59752d1f34ad4de790911a66
Bosseyang <bosse_yang@hotmail.com>
2025-09-04 15:38:51 +0200

    Update LandingPage.tsx

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,16 +1,14 @@
 import { useRandomCocktail } from "../hooks/useRandomCocktail";
 import { CocktailCard } from "../components/CocktailCard";
 import { Loader } from "../components/Loader";
+import { IconButton } from "../components/buttons/IconButton";
 
 export const LandingPage: React.FC = () => {
   const { cocktail, loading, fetchRandom } = useRandomCocktail();
 
   return (
     <main className="main landing-page">
-      <button onClick={fetchRandom}>
-        <img src="/cocktail.svg" />
-        <h2 className="h2 landing-page-h2">Random Cocktail</h2>
-      </button>
+      <IconButton onClick={fetchRandom} icon="refresh" name="Random Cocktail" />
       {loading && <Loader />}
       <div className={`load-wrapper ${loading ? "loading" : ""}`}>
         {cocktail && <CocktailCard cocktail={cocktail} />}
