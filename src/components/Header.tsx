5f50958307b1256b06838dff451913b60d5c212d
Bosseyang <bosse_yang@hotmail.com>
2025-09-04 15:38:09 +0200

    Update Header.tsx
    update Link to NavLink for better functionality

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,21 @@
-import { Link } from "react-router";
+import { Link, NavLink } from "react-router";
 
 export const Header: React.FC = () => {
   return (
     <header className="header">
       <nav className="header-nav">
-        <Link className="header-link" to="/">
+        <NavLink className="header-link" to="/">
           <h2>Home</h2>
-        </Link>
+        </NavLink>
         <Link className="header-link" to="/">
           <img src="/cocktail.svg" />
           <h1 className="h1 header-h1">Cocktails</h1>
+          {/* <img src="/poster.png" /> */}
         </Link>
 
-        <Link className="header-link" to="/search">
+        <NavLink className="header-link" to="/search">
           <h2>Search</h2>
-        </Link>
+        </NavLink>
       </nav>
     </header>
   );
