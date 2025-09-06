897a64fe4f2c850af2f88467bb3d45e6665be575
Bosseyang <bosse_yang@hotmail.com>
2025-09-06 18:46:47 +0200

    Update SearchPage.tsx

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,21 +1,21 @@
-import { SearchForm } from "../components/SearchForm";
 import { CocktailList } from "../components/CocktailList";
 import { Loader } from "../components/Loader";
-import { useCocktailSearch } from "../hooks/useCocktailSearch";
 import { Pagination } from "../components/Pagination";
 import { AdvancedSearchForm } from "../components/AdvancedSearchForm";
 import { useAdvancedSearch } from "../hooks/useAdvancedSearch";
 
 export const SearchPage: React.FC = () => {
   // const { handleSearch, loading, page, setPage, totalPages, paginated } = useCocktailSearch();
-  const { handleSearch, loading, page, setPage, totalPages, paginated } = useAdvancedSearch();
+  const { handleSearch, loading, page, setPage, totalPages, paginated, error } =
+    useAdvancedSearch();
 
   return (
     <section className="search-page">
       <h1 className="search-page-h1"></h1>
-      <SearchForm onSearch={(q) => handleSearch({ name: q })} />
+      {/* <SearchForm onSearch={(q) => handleSearch({ name: q })} /> */}
       <AdvancedSearchForm onSearch={handleSearch} />
       {loading && <Loader />}
+      {error && <p className="form-error-text">{error}</p>}
       <div className={`load-wrapper ${loading ? "loading" : ""}`}>
         <CocktailList cocktails={paginated} />
       </div>
