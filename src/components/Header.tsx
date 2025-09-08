147ff9588bdcf93427e5cc445eef706b52884f8d
Bosseyang <bosse_yang@hotmail.com>
2025-09-08 11:00:20 +0200

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,15 +4,14 @@ export const Header: React.FC = () => {
   return (
     <header className="header">
       <nav className="header-nav">
-        <NavLink className="header-link" to="/">
-          <h2>Home</h2>
-        </NavLink>
         <Link className="header-link" to="/">
           <img src="/cocktail.svg" />
           <h1 className="h1 header-h1">Cocktails</h1>
           {/* <img src="/poster.png" /> */}
         </Link>
-
+        <NavLink className="header-link" to="/">
+          <h2>Home</h2>
+        </NavLink>
         <NavLink className="header-link" to="/search">
           <h2>Search</h2>
         </NavLink>
