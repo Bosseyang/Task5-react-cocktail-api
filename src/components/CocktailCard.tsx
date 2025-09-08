275072692d4d2857216ebcb5fa23503bc2f9015b
Bosseyang <bosse_yang@hotmail.com>
2025-09-08 11:00:17 +0200

    Update CocktailCard.tsx

--- src/components/CocktailCard.tsx
+++ src/components/CocktailCard.tsx
@@ -1,19 +1,24 @@
 import { Link } from "react-router";
 import type { ICocktail } from "../types/types";
 import { CocktailImage } from "./CocktailImage";
+import { FavoriteButton } from "./buttons/FavoriteButton";
 
 interface ICocktailCardProps {
   cocktail: ICocktail;
+  cardSize?: string;
 }
 
-export const CocktailCard: React.FC<ICocktailCardProps> = ({ cocktail }) => {
+export const CocktailCard: React.FC<ICocktailCardProps> = ({ cocktail, cardSize }) => {
   return (
-    <Link to={`/cocktail/${cocktail.id}`} className="cocktail-card-link">
-      <article className="cocktail-card">
+    <article className="item cocktail-card-container">
+      <span className="card-favorite-position">
+        <FavoriteButton cocktail={cocktail} />
+      </span>
+      <Link to={`/cocktail/${cocktail.id}`} className={`cocktail-card cocktail-card-${cardSize}`}>
         <CocktailImage cocktail={cocktail} />
         <h2 className="cocktail-card-h2">{cocktail.name}</h2>
         <h3 className="hidden-h3">Click to see more 🢥</h3>
-      </article>
-    </Link>
+      </Link>
+    </article>
   );
 };
