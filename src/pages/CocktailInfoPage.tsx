ab72de9a8a483fda0a765715877b6ae901be3d86
Bosseyang <bosse_yang@hotmail.com>
2025-09-08 15:29:07 +0200

    Update CocktailInfoPage.tsx
    add favorite button for info page

--- src/pages/CocktailInfoPage.tsx
+++ src/pages/CocktailInfoPage.tsx
@@ -4,6 +4,7 @@ import { Loader } from "../components/Loader";
 import { CocktailIngredients } from "../components/CocktailInfo";
 import { useParams } from "react-router";
 import { getCocktailById } from "../services/cocktailApi";
+import { FavoriteButton } from "../components/buttons/FavoriteButton";
 
 export const CocktailInfoPage: React.FC = () => {
   const { id } = useParams<{ id: string }>();
@@ -26,6 +27,9 @@ export const CocktailInfoPage: React.FC = () => {
       <h2 className="info-page-h2">{cocktail.name}</h2>
       <div className="content info-page-wrapper">
         <div className="info-image-container">
+          <span className="info-favorite-position">
+            <FavoriteButton cocktail={cocktail} />
+          </span>
           {/* <CocktailImage cocktail={cocktail} /> */}
           <img className="cocktail-info-image" src={cocktail.thumbnail} />
         </div>
