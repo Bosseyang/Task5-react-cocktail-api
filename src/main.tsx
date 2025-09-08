0478600184f346f92343b7d11490ec9a2e78aaa7
Bosseyang <bosse_yang@hotmail.com>
2025-09-08 16:53:14 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router";
 import { routerJSX } from "./routerJSX.tsx";
 import "./css/index.css";
+import { Favorites } from "./components/features/favorites/Favorites.tsx";
+import { FavoriteContextProvider } from "./components/features/favorites/context/FavoriteContextProvider.tsx";
 
 createRoot(document.getElementById("root")!).render(
-  <RouterProvider router={routerJSX} />
+  <FavoriteContextProvider>
+    <RouterProvider router={routerJSX} />
+  </FavoriteContextProvider>
   // <StrictMode>
   //   <App />
   // </StrictMode>
