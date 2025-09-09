7c133fe2fa26be69d9267f88ed5b5d0bd7823a75
Bosseyang <bosse_yang@hotmail.com>
2025-09-09 14:59:44 +0200

    Update FavoritesPage.tsx

--- src/pages/FavoritesPage.tsx
+++ src/pages/FavoritesPage.tsx
@@ -9,6 +9,7 @@ export const FavoritesPage: React.FC = () => {
     <main className="favorites-page">
       <h2 className="favorites-h2">Your Favorite Cocktails</h2>
 
+      {favorites.length === 0 && <p>You don't have any favorites yet</p>}
 
       <div className="favorites-container">
         {favorites.map((cocktail) => (
