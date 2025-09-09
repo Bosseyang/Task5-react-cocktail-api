c00fd5f51d35d84cf08bf8761c27d9f023db615c
Bosseyang <bosse_yang@hotmail.com>
2025-09-09 16:01:31 +0200

    Create router.tsx

---
+++ src/app/router.tsx
@@ -0,0 +1,22 @@
+import { createBrowserRouter, createRoutesFromElements, Route } from "react-router";
+import App from "./App";
+import { LandingPage } from "../pages/LandingPage";
+import { CocktailInfoPage } from "../pages/CocktailInfoPage";
+import { SearchPage } from "../pages/SearchPage";
+import { FavoritesPage } from "../pages/FavoritesPage";
+import { IngredientPage } from "../pages/IngredientPage";
+
+// Data router with JSX routes.
+export const routerJSX = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index element={<LandingPage />}>
+        {/* <Route path=/> */}
+      </Route>
+      <Route path="/cocktail/:id" element={<CocktailInfoPage />} />
+      <Route path="/search" element={<SearchPage />} />
+      <Route path="/favorites" element={<FavoritesPage />} />
+      <Route path="/ingredients" element={<IngredientPage />} />
+    </Route>
+  )
+);
