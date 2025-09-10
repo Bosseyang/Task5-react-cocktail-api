be5a5bf1f62b6ab287a38afd6037a7d5d4931046
Bosseyang <bosse_yang@hotmail.com>
2025-09-10 15:50:40 +0200

    Update Header.tsx
    add mobile menu for header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,12 @@
 import { Link, NavLink } from "react-router";
+import { IconButton } from "./buttons/IconButton";
+import { useState } from "react";
 
 export const Header: React.FC = () => {
+  const [open, setOpen] = useState<boolean>(false);
+
+  const toggleMenu = () => setOpen((prev) => !prev);
+
   return (
     <header className="header">
       <nav className="header-nav">
@@ -9,15 +15,20 @@ export const Header: React.FC = () => {
           <h1 className="h1 header-h1">Cocktails</h1>
           {/* <img src="/poster.png" /> */}
         </Link>
-        <NavLink className="header-link" to="/">
-          <h2>Home</h2>
-        </NavLink>
-        <NavLink className="header-link" to="/search">
-          <h2>Search</h2>
-        </NavLink>
-        <NavLink className="header-link" to="/favorites">
-          <h2>Favorites</h2>
-        </NavLink>
+        <div className="desktop-nav">
+          <NavLink className="header-link" to="/">
+            <h2>Home</h2>
+          </NavLink>
+          <NavLink className="header-link" to="/search">
+            <h2>Search</h2>
+          </NavLink>
+          <NavLink className="header-link" to="/favorites">
+            <h2>Favorites</h2>
+          </NavLink>
+        </div>
+        <div className="mobile-nav">
+          <IconButton onClick={toggleMenu} icon={open ? "close" : "menu"} name="Menu" />
+        </div>
       </nav>
     </header>
   );
