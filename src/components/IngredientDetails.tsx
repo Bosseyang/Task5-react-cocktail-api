604e680c68c56cfddf4511b5c706da80238322a3
Bosseyang <bosse_yang@hotmail.com>
2025-09-10 11:22:46 +0200

    Create IngredientDetails.tsx
    moved from ingredient page

---
+++ src/components/IngredientDetails.tsx
@@ -0,0 +1,19 @@
+import type { IIngredientDetail } from "../types/types";
+import { InfoP } from "./InfoP";
+
+interface IIngredientDetailsProps {
+  ingredient: IIngredientDetail;
+}
+
+export const IngredientDetails: React.FC<IIngredientDetailsProps> = ({ ingredient }) => {
+  return (
+    <section className="section ingredient-details ">
+        <h3 className="ingredient-details-h3">Ingredient details</h3>
+        {ingredient.type && <InfoP label="Type: " info={ingredient.type} />}
+        <InfoP label="Alcoholic: " info={ingredient.isAlcoholic ? "Yes" : "No"} />
+        {ingredient.abv && <InfoP label="ABV: " info={`${ingredient.abv}%`} />}
+        {ingredient.description && <h4 className="">Ingredient description</h4>}
+        {ingredient.description && <p className="">{ingredient.description}</p>}
+    </section>
+  );
+};
