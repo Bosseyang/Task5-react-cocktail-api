793fb5f475fef70b5934ac99a567871c27ecd07b
Bosseyang <bosse_yang@hotmail.com>
2025-09-10 11:32:46 +0200

    Update useRandomCocktail.ts
    add local storage

--- src/hooks/useRandomCocktail.ts
+++ src/hooks/useRandomCocktail.ts
@@ -2,20 +2,26 @@ import { useEffect, useState } from "react";
 import { type ICocktail } from "../types/types";
 import { getRandomCocktail } from "../services/cocktailApi";
 
+const STORAGE_KEY = "random_cocktail";
+
 export function useRandomCocktail() {
   const [cocktail, setCocktail] = useState<ICocktail | null>(null);
   const [loading, setLoading] = useState<boolean>(false);
 
   async function fetchRandom() {
     setLoading(true);
-    const drink = await getRandomCocktail();
-    setCocktail(drink);
+    try {
+      const drink = await getRandomCocktail();
+      setCocktail(drink);
+      localStorage.setItem(STORAGE_KEY, JSON.stringify(drink));
+    } catch (err) {
+      console.error("Failed to fetch random cocktail:", err);
+    }
     setLoading(false);
   }
 
   useEffect(() => {
     fetchRandom();
   }, []);
-
   return { cocktail, loading, fetchRandom };
 }
