c30a04c12c0368a83e6826ce5191752043f1f3d1
Bosseyang <bosse_yang@hotmail.com>
2025-09-10 08:58:07 +0200

    Update CocktailCard.tsx

--- src/components/CocktailCard.tsx
+++ src/components/CocktailCard.tsx
@@ -5,16 +5,16 @@ import { FavoriteButton } from "./buttons/FavoriteButton";
 
 interface ICocktailCardProps {
   cocktail: ICocktail;
-  cardSize?: string;
+  cardSize?: "small" | "";
 }
 
-export const CocktailCard: React.FC<ICocktailCardProps> = ({ cocktail, cardSize }) => {
+export const CocktailCard: React.FC<ICocktailCardProps> = ({ cocktail, cardSize = "" }) => {
   return (
-    <article className="item cocktail-card-container">
+    <article className={`item cocktail-card-container ${cardSize}`}>
       <span className="card-favorite-position">
         <FavoriteButton cocktail={cocktail} />
       </span>
-      <Link to={`/cocktail/${cocktail.id}`} className={`cocktail-card cocktail-card-${cardSize}`}>
+      <Link to={`/cocktail/${cocktail.id}`} className={`cocktail-card ${cardSize}`}>
         <CocktailImage cocktail={cocktail} />
         <h2 className="cocktail-card-h2">{cocktail.name}</h2>
         <h3 className="hidden-h3">Click to see more 🢥</h3>
