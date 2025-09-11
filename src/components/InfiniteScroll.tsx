5b43d9dca719799f146cd7c62c64ff23081a3bfe
Bosseyang <bosse_yang@hotmail.com>
2025-09-11 16:05:51 +0200

    Update InfiniteScroll.tsx

--- src/components/InfiniteScroll.tsx
+++ src/components/InfiniteScroll.tsx
@@ -1,9 +1,36 @@
+import { useEffect, useRef, useState } from "react";
+import type { ICocktail } from "../types/types";
+import { Loader } from "./Loader";
+import { IconButton } from "./buttons/IconButton";
+interface IInfiniteScrollProps {
+  items: ICocktail[];
+  batchSize?: number;
+  view?: boolean;
+  imgList?: boolean;
+}
 export const InfiniteScroll: React.FC<IInfiniteScrollProps> = ({
   items,
   batchSize = 8,
 }) => {
+  const loaderRef = useRef<HTMLDivElement | null>(null);
   const [visibleCount, setVisibleCount] = useState(batchSize);
   useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        const target = entries[0];
+        if (target.isIntersecting) {
+          setVisibleCount((prev) => Math.min(prev + batchSize, items.length));
+        }
+      },
+      { threshold: 1.0, rootMargin: "0px" }
+    );
+
+
+    return () => {
+      if (loaderRef.current) {
+        observer.unobserve(loaderRef.current);
+      }
+    };
   }, [items, batchSize]);
   useEffect(() => {
     setVisibleCount(batchSize);
